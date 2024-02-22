fcaf53b89ec844365a62541a28d53497508dd5d7
jcwilliams2 <124515633+jcwilliams2@users.noreply.github.com>
2024-02-22 17:07:09 -0500

    Initialize starting player, computer, and game objects as well as opening game dialogue

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,5 @@
+import random
+
 empty_board = [["-", "-", "-"],
                ["-", "-", "-"],
                ["-", "-", "-"]]
@@ -5,6 +7,12 @@ empty_board = [["-", "-", "-"],
 placement_board = [[1, 2, 3],
                    [4, 5, 6],
                    [7, 8, 9]]
+
+def randomMark():
+    num = random.randint(1,2)
+    if num == 1:
+        return 'X'
+    return 'O'
 
 def printBoard(board):
     row_count = 0
@@ -28,19 +36,50 @@ def printBoard(board):
     print('\n')
 
 class Player:
-    def __init__(self, name, mark):
-        self.name = name
-        self.mark = mark
+    def __init__(self, name):
+        if name != "":
+            self.name = name
+        else:
+            self.name = "Player1"
+        self.mark = randomMark()
 
 class Computer:
-    def __init__(self, mark):
-
+    def __init__(self, player):
         self.name = "CP"
-        self.mark = mark
+        if player.mark == 'O':
+            self.mark = 'X'
+        else:
+            self.mark = 'O'
         
+#Game class keeps track of data from gameplay#       
+class Game:
+    def __init__(self, board):
+        self.board = board
 
-printBoard(placement_board)
+start = False
 
+#Start of Gameplay#
+print("Welcome to Tic-Tac-Toe!")
+answer = input("Would you like to play? y/n ")
+if(answer.lower() == 'y' or answer.lower() == 'yes'):
+    name = input("Input player name: ")
+    player = Player(name)
+    cp = Computer(player)
+    game = Game(placement_board)
+
+    print(f'\nHello {player.name}!\nTo begin, you are "{player.mark}".')
+    print('On your turn input the number in the area you would like to play.\nGood luck and here is your board!')
+
+    printBoard(game.board)
+    
+
+else:
+    print("Maybe another time. Goodbye.")
+
+#printBoard(placement_board)
+
+#player1 = Player('playerone')
+#com = Computer(player1)
 
 
 
