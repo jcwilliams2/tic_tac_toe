183fe0f7d23f77ac0c835513d2ae068a1fd8ab20
jcwilliams2 <124515633+jcwilliams2@users.noreply.github.com>
2024-03-01 11:17:15 -0500

    Add functionality to game play so that players have turns and game checks for a winner or a draw

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -16,7 +16,6 @@ def randomMark():
 
 def printBoard(board):
     row_count = 0
-    print('\n')
     for row in board:
         line = ""
         count = 0
@@ -32,6 +31,7 @@ def printBoard(board):
         row_count += 1    
         if(row_count < 3):
             print("___________")
+    print("\n")
 
 
 class Player:
@@ -41,6 +41,9 @@ class Player:
         else:
             self.name = "Player1"
         self.mark = randomMark()
+    
+    def __repr__(self):
+        return self.name
 
 class Computer:
     def __init__(self, player):
@@ -49,6 +52,9 @@ class Computer:
             self.mark = 'X'
         else:
             self.mark = 'O'
+
+    def __repr__(self):
+        return self.name
 
     def turn_position(self, game):
         possible = 0
@@ -60,44 +66,81 @@ class Computer:
         
 #Game class keeps track of data from gameplay#       
 class Game:
-    def __init__(self, board):
+    def __init__(self, board, player, computer):
         self.board = board
+        self.player = player
+        self.computer = computer
         self.playable = [1,2,3,4,5,6,7,8,9]
+        self.game_end = False
         self.turnCount = 0
+
+    def win(self):
+        if self.board[0][0] == self.board[0][1] == self.board[0][2]:
+            return True
+        elif self.board[1][0] == self.board[1][1] == self.board[1][2]:
+            return True
+        elif self.board[2][0] == self.board[2][1] == self.board[2][2]:
+            return True
+        elif self.board[0][0] == self.board[1][0] == self.board[2][0]:
+            return True
+        elif self.board[0][1] == self.board[1][1] == self.board[2][1]:
+            return True
+        elif self.board[0][2] == self.board[1][2] == self.board[2][2]:
+            return True
+        elif self.board[0][0] == self.board[1][1] == self.board[2][2]:
+            return True
+        elif self.board[0][2] == self.board[1][1] == self.board[2][0]:
+            return True
+        else:
+            return False
+        
+    def check_win(self, mark):
+        win = self.win()
+        if win:
+            self.game_end = True
+            if mark == self.player.mark:
+                print(f'{self.player} has won the game.')
+            else:
+                print(f'{self.computer} haswon the game.')
+        elif self.turnCount == 9:
+            self.game_end = True
+            print("Draw. Game Over.")
 
     def turn (self, place, mark):
         place_int = int(place)
         if place_int in self.playable:
             if place_int == 1:
                 self.board[0][0] = mark
-                self.playable.pop(0)
+                self.playable.remove(1)
             elif place_int == 2:
                 self.board[0][1] = mark
-                self.playable.pop(1)
+                self.playable.remove(2)
             elif place_int == 3:
                 self.board[0][2] = mark
-                self.playable.pop(2)
+                self.playable.remove(3)
             elif place_int == 4:
                 self.board[1][0] = mark
-                self.playable.pop(3)
+                self.playable.remove(4)
             elif place_int == 5:
                 self.board[1][1] = mark
-                self.playable.pop(4)
+                self.playable.remove(5)
             elif place_int == 6:
                 self.board[1][2] = mark
-                self.playable.pop(5)
+                self.playable.remove(6)
             elif place_int == 7:
                 self.board[2][0] = mark
-                self.playable.pop(6)
+                self.playable.remove(7)
             elif place_int == 8:
                 self.board[2][1] = mark
-                self.playable.pop(7)
+                self.playable.remove(8)
             else:
                 self.board[2][2] = mark
-                self.playable.pop(-1)
+                self.playable.remove(9)
 
             printBoard(self.board)
             self.turnCount += 1
+            if(self.turnCount > 4):
+                self.check_win(mark)
             #print(self.playable)
             return True
         else:
@@ -114,14 +157,38 @@ if(answer.lower() == 'y' or answer.lower() == 'yes'):
     name = input("Input player name: ")
     player = Player(name)
     cp = Computer(player)
-    game = Game(placement_board)
+    game = Game(placement_board, player, cp)
 
     print(f'\nHello {player.name}!\nTo begin, you are "{player.mark}".')
     print('On your turn input the number in the area you would like to play.\nGood luck and here is your board!')
-    position = input("Your Turn: ")
+    position = input("Your turn: ")
     game.turn(position, player.mark)
     cp_turn = cp.turn_position(game)
     game.turn(cp_turn, cp.mark)
+
+    position = input("\nYour turn: ")
+    game.turn(position, player.mark)
+    cp_turn = cp.turn_position(game)
+    game.turn(cp_turn, cp.mark)
+
+    position = input("\nYour turn: ")
+    game.turn(position, player.mark)
+
+    if game.game_end == False:
+        cp_turn = cp.turn_position(game)
+        game.turn(cp_turn, cp.mark)
+
+    if game.game_end == False:
+        position = input("\nYour turn: ")
+        game.turn(position, player.mark)
+    
+    if game.game_end == False:
+        cp_turn = cp.turn_position(game)
+        game.turn(cp_turn, cp.mark)
+
+    if game.game_end == False:
+        position = input("\nYour turn: ")
+        game.turn(position, player.mark)
 
 
 
